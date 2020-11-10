5df4102e0ce5a415c6265a6aa294962ac6d36c2f
Ubuntu <ubuntu@ip-172-31-4-227.ec2.internal>
2020-11-10 14:25:28 +0000

    p controller

--- PID.py
+++ PID.py
@@ -79,9 +79,11 @@ def timerCallBack(event):
     if state == 'state1':
         yaw = getAngle(odom)
         scan_len = len(scan.ranges)
+        print ("scan len")
         print (scan_len)
         if scan_len > 0:
             point = min (scan.ranges[scan_len-10 : scan_len+10]) 
+            print ("point")
             print (point)
             print (scan.ranges[scan_len-10])
             print (scan.ranges[scan_len+10])
